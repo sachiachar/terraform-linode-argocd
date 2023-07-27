5e01e3b73679cfd3a17293590bc643665e3720bd
Sachidananda Karkala <skarkala@akamai.com>
2023-07-27 14:09:08 +0800

    modified:   locals.tf

--- locals.tf
+++ locals.tf
@@ -3,6 +3,5 @@
 locals {
   root_dir               = dirname(abspath(path.root))
   kubeconf_path = "${local.root_dir}/.kube/kubeconfig.yaml"
-  install_script = "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/core-install.yaml"
-  
+  install_script = "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
 }
